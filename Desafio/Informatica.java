fad062b4e27e285cca58887471fb579201427ac7
lczinho <124937488+lczinho@users.noreply.github.com>
2024-06-23 17:33:14 -0300

    Add files via upload

---
+++ Desafio/Informatica.java
@@ -0,0 +1,24 @@
+public class Informatica extends Loja {
+    private double seguroEletronicos;
+
+    // Construtor
+    public Informatica(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, int capacidadeEstoque, double seguroEletronicos) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, capacidadeEstoque);
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    // Getters e Setters para taxaComercializacao
+    public double getSeguroEletronicos() {
+        return seguroEletronicos;
+    }
+
+    public void setSeguroEletronicos(double seguroEletronicos) {
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    // Sobrescrita do método toString para incluir a taxa de comercialização
+    public String toString(){
+        return "Nome é: " + getNome() + "a quantidade de funcionários é: " + getQuantidadeFuncionarios() + "e o salario base é: " + getSalarioBaseFuncionario() +
+        "o endereço é " + getEndereco() + "e a data de fundação é" + getDataFundacao() + "o seguro é " + getSeguroEletronicos();
+    }
+}
---
+++ Desafio/Vestuario.java
@@ -0,0 +1,24 @@
+public class Vestuario extends Loja {
+    private boolean produtosImportados;
+
+    // Construtor
+    public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, boolean produtosImportados, int capacidadeEstoque) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao, capacidadeEstoque);
+        this.produtosImportados = produtosImportados;
+    }
+
+    // Getters e Setters para taxaComercializacao
+    public boolean getProdutosImportados() {
+        return produtosImportados;
+    }
+
+    public void setProdutosImportados(boolean produtosImportados) {
+        this.produtosImportados = produtosImportados;
+    }
+
+    // Sobrescrita do método toString para incluir a taxa de comercialização
+    public String toString(){
+        return "Nome é: " + getNome() + "a quantidade de funcionários é: " + getQuantidadeFuncionarios() + "e o salario base é: " + getSalarioBaseFuncionario() +
+        "o endereço é " + getEndereco() + "e a data de fundação é" + getDataFundacao() + "o seguro é " + getProdutosImportados();
+    }
+}
